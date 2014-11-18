f226e77b5d60b65eead574b8c3191b790911fd18
=verlyntae <=verlyntae@gmail.com>
2014-11-18 11:08:33 -0800

    edited script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,20 @@
 //Function called "signIn", causes an alert to pop up with text "Thanks for signing in". Should be triggered by clicking on the button with id of "sign-in".
+function signIn() {
+	var message1 = "Thanks for signing in.";
+	alert(message1);
+}
 
+<button onclick="signIn();">sign-in</button>
 
 
 
 //Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "sign-up".
 
-function signIn() {
-	alert("Thanks for signing in");
+function signUp() {
+	var userInput = prompt("Please enter your email address");
+	document.write("Thanks for signing up, ");
+	document.getElementById("output-here").innerHTML=userInput;
 }
 
 
-<input type="button" value="sign-in" onclick="signIn()">
+<button onclick="getUserInput();">sign-up</button>
