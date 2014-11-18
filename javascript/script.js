e75d63a213ce56c7b17678c7814c175a9ed184a6
=verlyntae <=verlyntae@gmail.com>
2014-11-17 18:41:43 -0800

    Initialized repo

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+//Function called "signIn", causes an alert to pop up with text "Thanks for signing in". Should be triggered by clicking on the button with id of "sign-in".
+
+
+
+
+//Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "sign-up".
